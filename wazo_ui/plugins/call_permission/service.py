fadb2b1d08df6f36db58fa167a256ca7c639ddf5
Pascal Cadotte Michaud <pcm@wazo.io>
2020-05-08 08:00:23 -0400

    call_permissions: avoid removing and adding on update
    when updating a call_permission instead of removing all users, groups and
    outcalls only remove the ones that need to be removed and add the missing ones.

--- wazo_ui/plugins/call_permission/service.py
+++ wazo_ui/plugins/call_permission/service.py
@@ -38,24 +38,50 @@ class CallPermissionService(BaseConfdService):
     def update(self, resource):
         super().update(resource)
         existing_resource = self.get(resource['id'])
-        self.update_users(resource['id'], resource['user_uuids'], existing_resource['users'])
-        self.update_groups(resource['id'], resource['group_ids'], existing_resource['groups'])
-        self.update_outcalls(resource['id'], resource['outcall_ids'], existing_resource['outcalls'])
+        self.update_users(
+            resource['id'],
+            resource['user_uuids'],
+            self._extract_ids(existing_resource['users'], 'uuid'),
+        )
+        self.update_groups(
+            resource['id'],
+            resource['group_ids'],
+            self._extract_ids(existing_resource['groups'], 'id'),
+        )
+        self.update_outcalls(
+            resource['id'],
+            resource['outcall_ids'],
+            self._extract_ids(existing_resource['outcalls'], 'id'),
+        )
 
     def update_users(self, callpermission_id, user_uuids, existing_user_uuids):
-        for existing_user_uuid in existing_user_uuids:
+        add, remove = self.find_add_and_remove(user_uuids, existing_user_uuids)
+        for existing_user_uuid in remove:
             self._confd.users(existing_user_uuid).remove_call_permission(callpermission_id)
-        for user_uuid in user_uuids:
+        for user_uuid in add:
             self._confd.users(user_uuid).add_call_permission(callpermission_id)
 
-    def update_groups(self, callpermission_id, groups_ids, existing_groups_ids):
-        for existing_group_id in existing_groups_ids:
+    def update_groups(self, callpermission_id, group_ids, existing_group_ids):
+        add, remove = self.find_add_and_remove(group_ids, existing_group_ids)
+        for existing_group_id in remove:
             self._confd.groups(existing_group_id).remove_call_permission(callpermission_id)
-        for groups_id in groups_ids:
+        for groups_id in add:
             self._confd.groups(groups_id).add_call_permission(callpermission_id)
 
     def update_outcalls(self, callpermission_id, outcall_ids, existing_outcall_ids):
-        for existing_outcall_id in existing_outcall_ids:
+        add, remove = self.find_add_and_remove(outcall_ids, existing_outcall_ids)
+        for existing_outcall_id in remove:
             self._confd.outcalls(existing_outcall_id).remove_call_permission(callpermission_id)
-        for outcall_id in outcall_ids:
+        for outcall_id in add:
             self._confd.outcalls(outcall_id).add_call_permission(callpermission_id)
+
+    def find_add_and_remove(self, new, existing):
+        new_set = set(new or [])
+        existing_set = set(existing or [])
+        remove = list(existing_set - new_set)
+        add = list(new_set - existing_set)
+        return add, remove
+
+    @staticmethod
+    def _extract_ids(resources, id_field):
+        return [resource[id_field] for resource in resources]
