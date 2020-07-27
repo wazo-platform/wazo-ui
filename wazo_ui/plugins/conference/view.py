0ff5e0d6353f88167ce4e6df6347ce89761514d5
Francois Blackburn <fblackburn@wazo.community>
2020-07-27 14:01:36 -0400

    remove unused variable

--- wazo_ui/plugins/conference/view.py
+++ wazo_ui/plugins/conference/view.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from flask import jsonify, request
@@ -17,7 +17,6 @@ from .form import ConferenceForm
 
 class ConferenceView(BaseIPBXHelperView):
 
-    resource_name = 'conference'
     form = ConferenceForm
     resource = l_('conference')
 
