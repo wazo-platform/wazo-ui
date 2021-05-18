0ba5844914e5873869c1c0fa2adbde58aebe070e
Francois Blackburn <fblackburn@wazo.community>
2021-05-18 08:13:29 -0400

    rename working_instance_tenant to working_tenant
    why: legacy name

--- wazo_ui/helpers/tenant.py
+++ wazo_ui/helpers/tenant.py
@@ -9,4 +9,4 @@ from wazo_ui.core.client import engine_clients
 def refresh_instance_tenants():
     tenants = engine_clients['wazo_auth'].tenants.list()['items']
     session['instance_tenants'] = tenants
-    session['working_instance_tenant_uuid'] = tenants[0]['uuid'] if len(tenants) else None
+    session['working_tenant_uuid'] = tenants[0]['uuid'] if len(tenants) else None
