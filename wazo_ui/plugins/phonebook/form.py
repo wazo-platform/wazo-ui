5c71596e4a93e72cec0bc99636f63218258edf0b
Sylvain Boily <sylvain@wazo.io>
2021-05-18 17:02:57 -0400

    add description

--- wazo_ui/plugins/phonebook/form.py
+++ wazo_ui/plugins/phonebook/form.py
@@ -14,6 +14,7 @@ from wazo_ui.helpers.form import BaseForm
 
 class PhonebookForm(BaseForm):
     name = StringField(l_('Name'), [InputRequired(), Length(max=80)])
+    description = StringField(l_('Description'), [Length(max=80)])
     submit = SubmitField(l_('Submit'))
 
 
@@ -21,7 +22,7 @@ class ManagePhonebookForm(BaseForm):
     firstname = StringField(l_('Firstname'), [InputRequired(), Length(max=80)])
     lastname = StringField(l_('Lastname'), [InputRequired(), Length(max=80)])
     email = StringField(l_('Email'), [Length(max=80)])
-    phone = StringField(l_('Phone'), [Length(max=80)])
+    phone = StringField(l_('Phone'), [InputRequired(), Length(max=80)])
     mobile_phone = StringField(l_('Mobile Phone'), [Length(max=80)])
     fax = StringField(l_('Fax'), [Length(max=80)])
     phonebook_id = HiddenField()
