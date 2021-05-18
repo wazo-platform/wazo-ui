357e7a9704d9d112e03c87180cc086298a8cbdc1
Sylvain Boily <sylvain@wazo.io>
2021-05-18 15:45:28 -0400

    add contacts on global phonebook

--- wazo_ui/plugins/phonebook/plugin.py
+++ wazo_ui/plugins/phonebook/plugin.py
@@ -1,11 +1,11 @@
-# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2020-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from flask_menu.classy import register_flaskview
 from wazo_ui.helpers.plugin import create_blueprint
 
-from .service import PhonebookService
-from .view import PhonebookView
+from .service import PhonebookService, ManagePhonebookService
+from .view import PhonebookView, ManagePhonebookView
 
 phonebook = create_blueprint('phonebook', __name__)
 
@@ -19,5 +19,9 @@ class Plugin(object):
         PhonebookView.service = PhonebookService(clients['wazo_dird'])
         PhonebookView.register(phonebook, route_base='/phonebooks')
         register_flaskview(phonebook, PhonebookView)
+
+        ManagePhonebookView.service = ManagePhonebookService(clients['wazo_dird'])
+        ManagePhonebookView.register(phonebook, route_base='/manage_phonebooks')
+        register_flaskview(phonebook, ManagePhonebookView)
 
         core.register_blueprint(phonebook)
--- wazo_ui/plugins/phonebook/view.py
+++ wazo_ui/plugins/phonebook/view.py
@@ -2,11 +2,19 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from flask_babel import lazy_gettext as l_
+from requests.exceptions import HTTPError
+from flask_classful import route
+from flask import (
+    redirect,
+    render_template,
+    flash,
+    url_for
+)
 
 from wazo_ui.helpers.menu import menu_item
 from wazo_ui.helpers.view import BaseIPBXHelperView
 
-from .form import PhonebookForm
+from .form import PhonebookForm, ManagePhonebookForm
 
 
 class PhonebookView(BaseIPBXHelperView):
@@ -17,3 +25,59 @@ class PhonebookView(BaseIPBXHelperView):
     @menu_item('.ipbx.phonebooks.config', l_('Configuration'), order=1, icon="wrench", multi_tenant=True)
     def index(self):
         return super().index()
+
+
+class ManagePhonebookView(BaseIPBXHelperView):
+    form = ManagePhonebookForm
+    resource = 'phonebook'
+    settings = 'manage_phonebook'
+
+    @menu_item('.ipbx.phonebooks.manage', l_('Contacts'), order=2, icon="users", multi_tenant=True)
+    def index(self, form=None):
+        try:
+            resource = self.service.list_phonebook()['items'][0]
+            phonebook_id = resource['phonebook_id'] = resource['id']
+            resource_list = self.service.list_contacts(phonebook_id)
+        except HTTPError as error:
+            self._flash_http_error(error)
+            return redirect(url_for('admin.Admin:get'))
+
+        form = form or self._map_resources_to_form(resource)
+        form = self._populate_form(form)
+
+        kwargs = {
+            'form': form,
+            'resource_list': resource_list,
+            'phonebook_id': phonebook_id
+        }
+        if self.listing_urls:
+            kwargs['listing_urls'] = self.listing_urls
+        return render_template(self._get_template(self.settings), **kwargs)
+
+    def post(self):
+        form = self.form()
+        resources = self._map_form_to_resources_post(form)
+
+        if not form.csrf_token.validate(form):
+            self._flash_basic_form_errors(form)
+            return self._new(form)
+
+        try:
+            self.service.create_contact(resources)
+        except HTTPError as error:
+            form = self._fill_form_error(form, error)
+            self._flash_http_error(error)
+            return self._new(form)
+
+        flash(l_('%(resource)s: Resource has been created', resource=self.resource), 'success')
+        return self._redirect_referrer_or('index')
+
+    @route('/delete/<phonebook_id>/<id>', methods=['GET'])
+    def delete(self, phonebook_id, id):
+        try:
+            self.service.delete_contact(phonebook_id, id)
+            flash(l_('%(resource)s: Resource %(id)s has been deleted', resource=self.resource, id=id), 'success')
+        except HTTPError as error:
+            self._flash_http_error(error)
+
+        return self._redirect_referrer_or('index')
