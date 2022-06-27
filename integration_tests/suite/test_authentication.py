747dcb371a79e92df8549d81bee426588c3d4286
Francois Blackburn <fblackburn@wazo.community>
2022-06-27 09:55:43 -0400

    Fix selenium deprecated find_element_by_* usage
    why: this method was removed in selenium 4.3.0

--- integration_tests/suite/test_authentication.py
+++ integration_tests/suite/test_authentication.py
@@ -1,8 +1,9 @@
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from hamcrest import assert_that, contains_string, equal_to, not_, calling, raises
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 
 from .helpers.base import IntegrationTest
 from .helpers.constants import USERNAME_PASSWORD_ERROR
@@ -20,7 +21,7 @@ class TestLogin(IntegrationTest):
         login.fill_name('username', '')
         login.fill_name('password', '')
 
-        submit = login.driver.find_element_by_id('submit')
+        submit = login.driver.find_element(By.ID, 'submit')
         assert_that(submit.get_attribute('class'), contains_string('disabled'))
 
         login.save(waiting=False)
@@ -36,7 +37,7 @@ class TestLogin(IntegrationTest):
         login.fill_name('username', 'test')
         login.fill_name('password', 'foobar')
 
-        submit = login.driver.find_element_by_id('submit')
+        submit = login.driver.find_element(By.ID, 'submit')
         assert_that(submit.get_attribute('class'), not_(contains_string('disabled')))
 
         login.save(waiting=False)
