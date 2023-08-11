e1721895d5b453fa2d75dcaef873c847bacfbe63
Alexandre Fournier <afournier@wazo.io>
2023-08-11 10:35:18 -0400

    context: add temporary fix for UUID

--- wazo_ui/plugins/context/service.py
+++ wazo_ui/plugins/context/service.py
@@ -10,6 +10,13 @@ class ContextService(BaseConfdService):
     def __init__(self, confd_client):
         self._confd = confd_client
 
+    # NOTE(afournier): temporary fix for the non-usage of the UUID by the API
+    def list(self, *args, **kwargs):
+        results = super().list(*args, **kwargs)
+        for context in results['items']:
+            del context['uuid']
+        return results
+
     def update(self, context):
         super().update(context)
         included_contexts = [
