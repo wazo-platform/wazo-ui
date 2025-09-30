df30512ecc8814a26219cad08ec89b41ec4ea29f
Francois Blackburn <fblackburn@wazo.community>
2025-09-30 09:34:54 -0400

    bump flask-menu version and port helpers
    flask-menu remove flask-classy(classful) helpers, so we backport part of
    those in our helpers

--- wazo_ui/plugins/incall/plugin.py
+++ wazo_ui/plugins/incall/plugin.py
@@ -1,8 +1,7 @@
-# Copyright 2017-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from flask_menu.classy import register_flaskview
-
+from wazo_ui.helpers.menu import register_flaskview
 from wazo_ui.helpers.plugin import create_blueprint
 
 from .service import IncallService
--- wazo_ui/plugins/phone_number/plugin.py
+++ wazo_ui/plugins/phone_number/plugin.py
@@ -1,8 +1,7 @@
-# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2024-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from flask_menu.classy import register_flaskview
-
+from wazo_ui.helpers.menu import register_flaskview
 from wazo_ui.helpers.plugin import create_blueprint
 
 from .service import PhoneNumberService
